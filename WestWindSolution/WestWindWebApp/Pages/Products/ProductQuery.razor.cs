6ee804d7ecd94e1cfac996108fd89e8d5b07f08c
Sam Wu <swu@nait.ca>
2023-11-13 14:40:52 -0700

    End of Nov 13

---
+++ WestWindSolution/WestWindWebApp/Pages/Products/ProductQuery.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using WestWindSystem.BLL;   // for CategoryServices, ProductServices
+using WestWindSystem.Entities;   // for Category, Product
+
+namespace WestWindWebApp.Pages.Products
+{
+    public partial class ProductQuery
+    {
+        [Inject]
+        protected CategoryServices CurrentCategoryServices { get; set; }
+
+        private List<Category> categories = new ();
+
+        protected override void OnInitialized()
+        {
+            categories = CurrentCategoryServices.GetAll();
+        }
+
+        private void CategoryChanged()
+        {
+
+        }
+    }
+}
