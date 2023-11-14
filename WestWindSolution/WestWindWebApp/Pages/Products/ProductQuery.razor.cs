6b23ef508d4366cd2acf8c6dc774e42408bb2ca3
Sam Wu <swu@nait.ca>
2023-11-14 08:56:39 -0700

    End of Nov 14 class

--- WestWindSolution/WestWindWebApp/Pages/Products/ProductQuery.razor.cs
+++ WestWindSolution/WestWindWebApp/Pages/Products/ProductQuery.razor.cs
@@ -9,15 +9,69 @@ namespace WestWindWebApp.Pages.Products
         [Inject]
         protected CategoryServices CurrentCategoryServices { get; set; }
 
-        private List<Category> categories = new ();
+        [Inject]
+        protected ProductServices CurrentProductServices { get; set; }
+
+        private List<Category> categories = new (); // For populating the categories select element
+
+        private int selectedCategoryId = 0; // The current seleced category
+      
+        private List<Product> products = new (); // products for a selected category
+
+        private string? feedbackMessage;
+
+        private string? productName;
 
         protected override void OnInitialized()
         {
             categories = CurrentCategoryServices.GetAll();
         }
 
-        private void CategoryChanged()
+        private void OnCategoryChanged(ChangeEventArgs e)
         {
+            feedbackMessage = null;
+            selectedCategoryId = int.Parse(e.Value.ToString());
+            if (selectedCategoryId == 0)
+            {
+                products.Clear ();
+            }
+            else
+            {
+                try
+                {
+                    products = CurrentProductServices.GetByCategoryId(selectedCategoryId);
+                }
+                catch (Exception ex)
+                {
+                    feedbackMessage = $"Error fetch catching cateories with exception: {ex.Message}";
+                }
+            }
+
+        }
+
+        private void OnProductNameSearch()
+        {
+            feedbackMessage = null;
+            if (string.IsNullOrWhiteSpace(productName))
+            {
+                feedbackMessage = "You must enter at least one character to serach for.";
+                products.Clear();
+            }
+            else
+            {
+                try
+                {
+                    products = CurrentProductServices.GetByProductName(productName);
+                    if (products.Count == 0)
+                    {
+                        feedbackMessage = $"There are products with a product name of {productName}";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    feedbackMessage = $"Error fetch products with exception: {ex.Message}";
+                }
+            }
 
         }
     }
